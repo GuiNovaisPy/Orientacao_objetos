ee8b4e54941c16b1230921a9fb491f4c22aa2f26
GuiNovaisPy <guinovais0702@gmail.com>
2024-05-23 20:20:34 -0300

    Feat: Implementa o metodo especial __str__ afim de dar um retorno diferente do objeto na variavel de instancia do mesmo, podendo escolher o que a class devolve de acordo com os atributos disponiveis

--- restaurante.py
+++ restaurante.py
@@ -4,14 +4,25 @@ class Restaurante:
     
     def __init__(self,nome,categoria) -> None: 
         
-        #O init é um metodo contrutor que é chamado quando instanciamos o nosso objeto,neste caso os args que passamos para o init, são obrigatorios e devem ser passados ao instanciar o nosso objeto
+        #O __init__ é um metodo contrutor que é chamado quando instanciamos o nosso objeto,neste caso os args que passamos para o init, são obrigatorios e devem ser passados ao instanciar o nosso objeto
         
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
         
+    def __str__(self) -> str:
+        
+        # o __str__ é um metodo especial que te permite alocar algo que voce tenha de atributo no seu objeto e queira devolver ele na variavel de instancia do objeto
+        return self.nome
+        
 
 restaurante_1 = Restaurante(nome='Restaurante 1',categoria='Gourmet')
+print(restaurante_1)# printa oq for devolvido no metodo especial __str__ e se ele não existe printa o objeto e o espaço da memoria onde ele esta alocado
+
+restaurante_1.nome = "Pizzaria"
+print(restaurante_1)
+
+
 print(f'''
 Nome restaurante: {restaurante_1.nome}
 Categoria restaurate: {restaurante_1.categoria}

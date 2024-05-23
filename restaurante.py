84dd64684e809cc3448305db84266816cf387e1a
GuiNovaisPy <guinovais0702@gmail.com>
2024-05-23 19:55:30 -0300

    Feat:Criando restaurante

---
+++ restaurante.py
@@ -0,0 +1,16 @@
+
+
+class Restaurante:
+    
+    nome = str
+    categoria = str
+    ativo = bool
+    
+restaurante_1 = Restaurante()
+
+restaurante_1.categoria = "Gourmet"
+restaurante_1.ativo  = True
+
+print(restaurante_1.nome)
+
+

abb9fc44b53d61da1e3164c3cd3b2ea82378beb2
GuiNovaisPy <guinovais0702@gmail.com>
2024-05-23 20:09:56 -0300

    Feat: Implementa o metodo contrutor __init__ afim de obrigar os atributos necessario sempre que o objeto for instanciado

--- restaurante.py
+++ restaurante.py
@@ -2,15 +2,22 @@
 
 class Restaurante:
     
-    nome = str
-    categoria = str
-    ativo = bool
-    
-restaurante_1 = Restaurante()
+    def __init__(self,nome,categoria) -> None: 
+        
+        #O init é um metodo contrutor que é chamado quando instanciamos o nosso objeto,neste caso os args que passamos para o init, são obrigatorios e devem ser passados ao instanciar o nosso objeto
+        
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        
 
-restaurante_1.categoria = "Gourmet"
-restaurante_1.ativo  = True
+restaurante_1 = Restaurante(nome='Restaurante 1',categoria='Gourmet')
+print(f'''
+Nome restaurante: {restaurante_1.nome}
+Categoria restaurate: {restaurante_1.categoria}
+Ativo? {restaurante_1.ativo}
+''')
 
-print(restaurante_1.nome)
+
 
 

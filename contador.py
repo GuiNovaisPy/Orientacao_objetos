888eb17c3a2078032bb56a20d104f291664827d6
GuiNovaisPy <guinovais0702@gmail.com>
2024-05-24 00:32:04 -0300

    Fix: correção do Contador

--- contador.py
+++ contador.py
@@ -7,15 +7,25 @@ class Contador:
     contador_global = 0
 
     def __init__(self):
-        self.valor = 0
+        pass
 
-    def __str__(self):
-        return f'Contador: {self.valor}'
+    @classmethod
+    def __str__(cls):
+        return f'Contador: {cls.contador_global}'
 
-    def incrementar(self):
-        self.valor += 1
+    @classmethod
+    def incrementar(cls):
+        cls.contador_global += 1
 
     @classmethod
     def zerar_contador_global(cls):
         cls.contador_global = 0
         return 'Contador global foi zerado.'
+ 
+ 
+i =  Contador()  
+i.incrementar()
+i.incrementar()
+i.zerar_contador_global()
+    
+print(i.contador_global)

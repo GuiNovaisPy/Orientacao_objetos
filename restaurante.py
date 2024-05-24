520328881e7a03243f6f04ac3c84787e512d8152
GuiNovaisPy <guinovais0702@gmail.com>
2024-05-23 23:43:45 -0300

    Feat: adicionando atributos privados

--- restaurante.py
+++ restaurante.py
@@ -7,21 +7,21 @@ class Restaurante:
         
         #O __init__ é um metodo contrutor que é chamado quando instanciamos o nosso objeto,neste caso os args que passamos para o init, são obrigatorios e devem ser passados ao instanciar o nosso objeto
         
-        self.nome = nome
-        self.categoria = categoria
-        self._ativo = ativo
+        self._nome = nome.title()
+        self._categoria = categoria.title()
+        self._ativo = ativo #_define um metodo privado ou seja o valor destes atributos nao podem ser mudados do lado de fora do objeto por mais que ele esteja instanciado
         Restaurante.restaurantes.append(self)
         
     def __str__(self) -> str:
         
         # o __str__ é um metodo especial que te permite alocar algo que voce tenha de atributo no seu objeto e queira devolver ele na variavel de instancia do objeto
-        return self.nome
+        return self._nome
     
     
     def listar_restaurantes(self) -> None:
         for item in Restaurante.restaurantes:
-            print(item.nome)
-            print(item.categoria)
+            print(item._nome)
+            print(item._categoria)
             print(item.ativo)
             print(60*"-")
             
@@ -47,4 +47,6 @@ restaurante_1 = Restaurante(nome='Restaurante 1',categoria='Gourmet',ativo=False
 restaurante_2 = Restaurante(nome='Restaurante 2',categoria='bar',ativo=True)
 
 restaurante_1.listar_restaurantes()
+
+
 

aff234ec949b512b4350b643d8feaef462d701b6
GuiNovaisPy <guinovais0702@gmail.com>
2024-05-24 00:23:35 -0300

    Style: Espaços desnecessarios retirados

--- restaurante.py
+++ restaurante.py
@@ -4,7 +4,6 @@ class Restaurante:
     restaurantes = [] #É um atributo da class
     
     def __init__(self,nome,categoria,ativo) -> None: 
-        
         """O __init__ é um metodo contrutor que é chamado quando instanciamos o nosso objeto,
         neste caso os args que passamos para o init, são obrigatorios e devem ser passados ao instanciar o nosso objeto.
         
@@ -18,17 +17,14 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
         
     def __str__(self) -> str:
-        
         """o __str__ é um metodo especial que te permite alocar algo que voce
         tenha de atributo no seu objeto e queira devolver ele na variavel de instancia do objeto"""
         
         return self._ativo
     
     
-    
     @classmethod
     def listar_restaurantes(cls) -> None:
-        
         """@classmethod é um decoretor utilizado para especificar que o metodo é um metodo referenciado a classe,
         não ao objeto que esta classe constrói, ou seja é um metodo que pode ser chamado direto sem instancia a classe.
         
@@ -40,7 +36,6 @@ class Restaurante:
         
         """
         
-        
         for item in cls.restaurantes:
             print(item._nome)
             print(item._categoria)
@@ -50,19 +45,14 @@ class Restaurante:
             
     @property
     def ativo(self) -> str:
-        
         """@property é um Decoretor do python que modifica como o atributo vai ser lido e acessado
         sempre que o atributo for ACESSADO. """
           
         return 'Ativo' if self._ativo is True else 'Inativo'
     
-    
-    
     def alternar_estado(self):
-        
         """Funcao para alternar estado do restaurante.
         negar um valor como abaixo inverte o valor bool atual"""
-        
         
         self._ativo = not self._ativo 
     
